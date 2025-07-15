df0f4d4e04d99c522236148123137431f96d77f6
Yahya Riswan Pk <pk.yahyariswan@gmail.com>
2025-07-15 09:14:22 +0530

    Delete mt3 directory

--- mt3/a/qn1.js
+++
@@ -1,8 +0,0 @@
-function  invertArray(arr){
-    for(let i=0; i < arr.length;i++){
-        arr[i] = arr[i] * -1
-    }
-    return arr;
-}
-
-console.log(invertArray([1, 2, 3, 4, 5]))
--- mt3/b/qn1.js
+++
@@ -1,8 +0,0 @@
-function arrayOfMultiples(num,n){
-    let arr = []
-    for(let i = 1; i <= n; i++){
-        arr.push(num * i)
-    }
-    return arr;
-}
-console.log(arrayOfMultiples(7, 5))
